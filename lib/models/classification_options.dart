8c3bec2772c0c453334e9335d377ba646054aa5d
leynier <leynier41@gmail.com>
2021-02-02 02:56:42 -0500

    Implement mock on extensor

--- lib/models/classification_options.dart
+++ lib/models/classification_options.dart
@@ -1,5 +1,30 @@
 import 'package:json_annotation/json_annotation.dart';
 
+extension CausesTagsExt on CausesTags {
+  String toStr() {
+    switch(this) {
+      case CausesTags.Quantity:
+        // TODO: Handle this case.
+        break;
+      case CausesTags.Deceased:
+        // TODO: Handle this case.
+        break;
+      case CausesTags.Distance:
+        // TODO: Handle this case.
+        break;
+      case CausesTags.LowQuality:
+        // TODO: Handle this case.
+        break;
+      case CausesTags.Price:
+        // TODO: Handle this case.
+        break;
+      case CausesTags.NoDir:
+        // TODO: Handle this case.
+        break;
+    }
+  }
+}
+
 enum CausesTags {
   @JsonValue('causes_quantity')
   Quantity,
